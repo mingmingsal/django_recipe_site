00f50154c78bb6b65fc33d23d1702b71c548a3da
mingmingsal <miguellorenzosalvador@gmail.com>
2024-08-09 14:22:15 +0800

    Added steps to details view

--- recipes/admin.py
+++ recipes/admin.py
@@ -6,12 +6,14 @@ from .models import Recipe, Step, Ingredient
 class IngredientInLine(admin.TabularInline):
     model = Ingredient
     extra = 1
-
+class StepInLine(admin.StackedInline):
+    model = Step
+    extra = 1
 class RecipeAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,{"fields":["recipe_name"]}),
         ("Date Information", {"fields":["pub_date"]}),
         ("Recipe Description", {"fields":["recipe_description"]})
     ]
-    inlines = [IngredientInLine]
+    inlines = [IngredientInLine, StepInLine]
 admin.site.register(Recipe, RecipeAdmin)
