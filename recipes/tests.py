dc9dba9310de9ee3fe068672327983578664e15f
mingmingsal <miguellorenzosalvador@gmail.com>
2024-08-09 15:39:43 +0800

    added 2 tests for model

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Recipe,Ingredient,Step
+
+#Helpers
+def create_recipe(recipe_name, daysOffset, steps, ):
+
+    time = timezone.now() + datetime.timedelta(days=daysOffset)
+    return Recipe.objects.create(recipe_name=recipe_name, pub_date=time)
+
+def create_step(step, recipe_name):
+    return Step.objects.create(step_description=step, recipe=recipe_name)
+
+#Model Tests
+class RecipeModelTests(TestCase):
+    def test_make_blank_recipe(self):
+        time = timezone.now()
+        recipe = Recipe(recipe_name="hotdog",pub_date=time)
+        self.assertIs(recipe.recipe_name=="hotdog", True)
+    def test_add_step(self):
+        time = timezone.now()
+        new_recipe = Recipe(recipe_name="hotdog",pub_date=time)
+        new_step = Step(step_description="Steam bun",recipe=new_recipe)
+        self.assertIs(new_step.recipe==new_recipe, True)
+
+# View Tests
+"""
+class IndexViewTests(TestCase):
+    def test_step_add(self):
+        response = self.client.get(reverse("recipes:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No recipes are available.")
+        self.assertQuerySetEqual(response.context["recipe)_list"], [])
+
+class DetailsViewTests(TestCase):
+    def test_step_add(self):
+        recipe = create_recipe(question_text="Ice Cream Sundae.", days=0)
+        step = create_step("Scoop Ice Cream",recipe)
+        url = reverse("recipes:detail", args=(recipe.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+"""
